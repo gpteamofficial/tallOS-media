407f2bae0c7358e9434275c2c2f25d9fe19839e6
GP Team <omar9dev@gmail.com>
2026-01-06 18:12:09 +0200

    Create  micro-installer.sh

---
+++ micro-installer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# micro-installer.sh
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+  exec sudo bash -s "$@" < /dev/stdin
+fi
+
+cd /tmp
+curl -fsSL https://getmic.ro | bash >/dev/null 2>&1
+install -m 755 micro /usr/bin/micro
+rm -f micro
