92d826a04dabfe7c02dda2cc2003601054eff4d7
GP Team <omar9dev@gmail.com>
2026-01-06 18:10:43 +0200

    Update micro-installer.sh

--- micro-installer.sh
+++ micro-installer.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 if [ "$EUID" -ne 0 ]; then
@@ -10,4 +9,3 @@ cd /tmp
 curl -fsSL https://getmic.ro | bash >/dev/null 2>&1
 install -m 755 micro /usr/bin/micro
 rm -f micro
-echo "Done Installing Micro"
