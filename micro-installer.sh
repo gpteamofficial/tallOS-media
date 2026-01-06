86f1edf48b022880674299e3ee6df703c793ce08
GP Team <omar9dev@gmail.com>
2026-01-06 18:15:47 +0200

    Update micro-installer.sh

--- micro-installer.sh
+++ micro-installer.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# micro-installer.sh
 set -e
 
-if [ "$EUID" -ne 0 ]; then
-  exec sudo bash -s "$@" < /dev/stdin
-fi
+TMP_DIR="$(mktemp -d)"
+cd "$TMP_DIR"
 
-cd /tmp
-curl -fsSL https://getmic.ro | bash >/dev/null 2>&1
-install -m 755 micro /usr/bin/micro
-rm -f micro
+curl -fsSL https://getmic.ro | bash
+
+sudo install -m 755 micro /usr/bin/micro
+
+cd /
+rm -rf "$TMP_DIR"
